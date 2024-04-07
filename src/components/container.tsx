77faa3ae1501cbf4090f7b0b8dd2d8dec8b17663
SARGA(labs)© <outdatedgfx@gmail.com>
2024-04-07 06:20:46 +0530

    init: SARGAlabs

---
+++ src/components/container.tsx
@@ -0,0 +1,16 @@
+import { clsx } from 'clsx'
+import * as React from 'react'
+
+export const Container = React.forwardRef<
+  HTMLDivElement,
+  JSX.IntrinsicElements['div'] & {
+    as?: 'div' | 'section'
+  }
+>(({ className, as = 'div', ...props }, ref) => {
+  const Element: React.ElementType = as
+  return (
+    <Element {...props} className={clsx('container', className)} ref={ref} />
+  )
+})
+
+export type ContainerProps = React.ComponentProps<typeof Container>
