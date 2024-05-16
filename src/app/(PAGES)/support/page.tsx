22e38c32d2cebd2c831159610fb7248206d4319a
SARGA(labs)© <outdatedgfx@gmail.com>
2024-05-16 14:39:21 +0530

    add: Support Page

---
+++ src/app/(PAGES)/support/page.tsx
@@ -0,0 +1,20 @@
+import React from 'react'
+
+const SUPPORTPAGE = () => {
+  return (
+    <>
+      <h1>SUPPORT</h1>
+      <p>
+        For any queries, please contact us at{' '}
+        <a
+          href="mailto:frontdesk@sargalabs.co"
+          style={{ textDecoration: 'underline' }}
+        >
+          FRONTDESK@SARGALABS.CO
+        </a>
+      </p>
+    </>
+  )
+}
+
+export default SUPPORTPAGE
